2a1bf9cddd0b0bb973cefb9b34df58880a2d2ed3
armanbin007 <160709760+armanbin007@users.noreply.github.com>
2024-10-17 16:41:58 +0600

    Hello World

---
+++ Prac 2/2_arithmetic-operators.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a = 6, b = 3;
+    int c = a + b;
+    
+    printf("The value of a is %d\nThe value of b is %d\nSum is %d\n", a, b, c);
+    printf("TThe remainder of a/b is %d", a%b);
+
+    return 0;
+}

746618db5be9e93ee3a57a9c8715cbce717eef27
armanbin007 <160709760+armanbin007@users.noreply.github.com>
2024-10-17 17:03:04 +0600

    .

--- Prac 2/3_type_conversion.c
+++ Prac 2/3_type_conversion.c
@@ -4,6 +4,6 @@ int main()
     int a=9;
     float b=2;
     float c= a/b;
-    printf("The value of a/b is %f",c);
+    printf("The value of a/b is %.2f",c);
     return 0;
 }
---
+++ Prac 2/quiz.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    float k= 3.0/9.0;
+    printf("Value of k is %f", k);
+    return 0;
+}

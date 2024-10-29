65e1adb9b80aed845f5b4e9c471c393c06dfa1f7
armanbin007 <160709760+armanbin007@users.noreply.github.com>
2024-10-29 20:21:37 +0600

    Delete Prac 2 directory

--- Prac 2/3_type_conversion.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int a=9;
-    float b=2;
-    float c= a/b;
-    printf("The value of a/b is %.2f",c);
-    return 0;
-}
--- Prac 2/quiz.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    float k= 3.0/9.0;
-    printf("Value of k is %f", k);
-    return 0;
-}

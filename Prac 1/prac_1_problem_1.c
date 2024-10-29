57a9574b6170a5dc23d6e08f7556a39251a03af8
armanbin007 <160709760+armanbin007@users.noreply.github.com>
2024-10-29 20:21:27 +0600

    Delete Prac 1 directory

--- Prac 1/prac_1_problem_1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    int lenght,width;
-    printf("Input the value of lenght and width: ");
-    scanf("%d %d", &lenght, &width);
-    printf("The area of this rectangular is %d", lenght*width);
-    return 0;
-}
--- Prac 1/prac_1_problem_3.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    float C,F;
-    scanf("%f", &C);
-    F = C*9/5 + 32;
-    printf("Celcius to Fahrenheit is: %.2f", F);
-    return 0;
-}
